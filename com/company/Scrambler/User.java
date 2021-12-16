233834bb24d967dcb14f8c4db513d00d662a215a
Alexander Duke <9803348@gmail.com>
2021-12-16 18:10:27 +0300

    GuessGame version 1

---
+++ com/company/Scrambler/User.java
@@ -0,0 +1,14 @@
+package com.company.Scrambler;
+
+public class User {
+    String name;
+    int age;
+
+    String city = "null";
+    String street = "null";
+    int numHouse;
+    String address = city + " " + street + " " + numHouse;
+
+    char gender;
+    String password;
+}
