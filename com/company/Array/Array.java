5a06339ceacad035be154163587f801843cf63e6
Alexander Duke <9803348@gmail.com>
2021-12-23 16:55:15 +0300

    refactoring into create and print methods

---
+++ com/company/Array/Array.java
@@ -0,0 +1,69 @@
+package com.company.Array;
+
+import java.util.Scanner;
+import java.lang.Math;
+import static com.company.Array.constants.ArrayMessages.*;
+
+public class Array {
+    private final Scanner scanner = new Scanner(System.in);
+    private String usersInput = "";
+    private boolean isIncorrectInput;
+    private int numberOfColumns, numberOfLines;
+
+
+
+    public void startArray() {
+        System.out.println(WELCOME_MESSAGE);
+        while (!usersInput.contains(EXIT_MESSAGE)) {
+            isIncorrectInput = true;
+            char[][] array = createMatrix();
+            fillingTheMatrixRandomChar(array);
+            printMatrix(array);
+            System.out.println(RETRY_OR_FINISH_MESSAGE);
+            usersInput = scanner.nextLine();
+            usersInput = scanner.nextLine();
+        }
+    }
+
+    private void fillingTheMatrixRandomChar(char[][] array) {
+        for (int i = 0; i < numberOfLines; i++) {
+            for (int j = 0; j < numberOfColumns; j++) {
+                int num = randomWithRange(33,126);
+                char charNum = (char) num;
+                array[i][j] = charNum;
+            }
+        }
+    }
+
+    private void printMatrix(char[][] array) {
+        for (int i = 0; i < numberOfLines; i++) {
+            for (int j = 0; j < numberOfColumns; j++) {
+                System.out.printf("%3c",array[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
+    private char[][] createMatrix() {
+        System.out.print(ENTER_THE_NUMBER_OF_COLUMNS);
+        enterTheNumberOfColumns();
+        System.out.print(ENTER_THE_NUMBER_OF_LINES);
+        enterTheNumberOfLines();
+        char[][] array = new char[numberOfLines][numberOfColumns];
+        return array;
+    }
+
+    private void enterTheNumberOfLines() {
+        numberOfLines = scanner.nextInt();
+    }
+
+    private void enterTheNumberOfColumns() {
+        numberOfColumns = scanner.nextInt();
+    }
+
+    int randomWithRange(int min, int max){
+
+        int range = (max - min) + 1;
+        return (int)(Math.random() * range) + min;
+    }
+}
---
+++ com/company/Array/constants/ArrayMessages.java
@@ -0,0 +1,9 @@
+package com.company.Array.constants;
+
+public class ArrayMessages {
+    public static final String WELCOME_MESSAGE = "This program creates a 2d array of any size";
+    public static final String EXIT_MESSAGE = "exit";
+    public static final String ENTER_THE_NUMBER_OF_COLUMNS = "Enter the number of columns\n";
+    public static final String ENTER_THE_NUMBER_OF_LINES = "Enter the number of lines\n";
+    public static final String RETRY_OR_FINISH_MESSAGE = "\nPress Enter to retry or enter an \"exit\" to complete.";
+}
